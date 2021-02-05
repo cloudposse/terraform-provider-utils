5c7acf0f2d4e3664628e8836ccd0e58f47527864
aknysh <andriy.knysh@gmail.com>
2021-02-05 10:03:23 -0500

    Updates

--- internal/convert/slice.go
+++ internal/convert/slice.go
@@ -1,10 +1,17 @@
 package convert
 
+import "github.com/pkg/errors"
+
 // SliceOfInterfacesToSliceOfStrings takes a slice of interfaces and converts it to a slice of strings
 func SliceOfInterfacesToSliceOfStrings(input []interface{}) ([]string, error) {
+	if input == nil {
+		return nil, errors.New("imput must not be nil")
+	}
+
 	output := make([]string, 0)
 	for _, current := range input {
 		output = append(output, current.(string))
 	}
+
 	return output, nil
 }
