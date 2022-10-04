8f0f414940473396e4e82ccf84b36aafccda1675
aknysh <andriy.knysh@gmail.com>
2022-10-03 20:11:09 -0400

    Update `utils_component_config` data source. Bump `atmos` version

--- examples/data-sources/utils_component_config/outputs.tf
+++ examples/data-sources/utils_component_config/outputs.tf
@@ -7,3 +7,8 @@ output "output2" {
   value       = local.result2
   description = "Component config from provided context"
 }
+
+output "output3" {
+  value       = local.result3
+  description = "Component config from provided stack using `atmos_cli_config_path` and `atmos_base_path` variables"
+}
